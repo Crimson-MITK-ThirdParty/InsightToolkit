0b008f85216729cb9397576f690a685284683f4c
ajm <ajm@a>
2021-05-24 10:25:41 -0400

    Contents of MITK third party archive

---
+++ Modules/Remote/WikiExamples.remote.cmake
@@ -0,0 +1,10 @@
+#
+# ITK WikiExamples
+#
+
+itk_fetch_module(WikiExamples
+  "A collection of examples that illustrate how to use ITK."
+  GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKWikiExamples.git
+  # January 20, 2016
+  GIT_TAG a34ad855f68996cf289b94af5e3a8fdb375cd170
+  )
